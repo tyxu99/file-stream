f3562c4ae7eb9cc020501f9ea4358a7e94a727fb
tyxu99 <uasic@outlook.com>
2023-06-07 17:00:20 +0800

    feat: test

--- src/controllers/fileDownload.controller.ts
+++ src/controllers/fileDownload.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Res, StreamableFile } from '@nestjs/common';
 import { Response } from 'express';
 import { join } from 'path';
 import * as fs from 'fs';
+import * as process from 'process';
 // import { AppService } from '../app.service';
 
 @Controller('fileStream')
@@ -9,35 +10,21 @@ export class FileDownloadController {
   // constructor(private readonly appService: AppService) {}
 
   @Get(':filename')
-  downloadFile(@Param('filename') filename: string, @Res() res: Response) {
-    fs.readdir(join('./uploads'), (err, files) => {
-      if (err) {
-        res.send({ data: 'no such file' });
-      }
-
-      if (files.includes(filename)) {
-        const filePath = join('./uploads/', filename);
-        const file = fs.createReadStream(filePath);
-        const stat = fs.statSync(filePath);
-        res.setHeader('Content-Length', stat.size);
-        res.setHeader('Content-Type', 'images/png');
-        res.setHeader(
-          'Content-Disposition',
-          'attachment; filename=' + filename,
-        );
-        res.set({
-          'Content-Length': stat.size,
-          'Content-Type': 'images/png',
-          'Content-Disposition': 'attachment; filename=' + filename,
-        });
-        // return new StreamableFile(file);
-
-        file.pipe(res);
-        // res.send({ [filename]: files });
-        // console.log(files);
-      } else {
-        res.send({ data: 'no such file' });
-      }
-    });
+  downloadFile(
+    @Param('filename') filename: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const filePath = join('./uploads', filename);
+    if (fs.existsSync(filePath)) {
+      res.set({
+        'Content-Type': 'image/png',
+        'Content-Disposition': `attachment; filename="${filename}"`,
+      });
+      const fileStream = fs.createReadStream(filePath);
+      const streamFile = new StreamableFile(fileStream);
+      return streamFile;
+    } else {
+      res.send({ data: 'no such file' });
+    }
   }
 }
